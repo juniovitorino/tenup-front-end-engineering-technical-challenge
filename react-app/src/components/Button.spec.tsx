ef13fc7b74210c51d908d9512fde1a9022313224
Junio Vitorino <jgvitorino@gmail.com>
2023-04-23 09:15:25 -0300

    feat|refactor: Convert Anchor Component to Button

--- react-app/src/components/Button.spec.tsx
+++ react-app/src/components/Button.spec.tsx
@@ -1,6 +1,36 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import Button from './Button'
 
-describe('Button', () => {
-  it.todo("should render the button");
-})
+describe('Anchor Link', () => {
+  beforeEach(() => { });
+
+  it('should render the anchor link', () => {
+    render(<Button href="https://example.com" title="Example Website" ariaLabel="Learn more about Example Website">Anchor Text</Button>);
+    const anchorElement = screen.getByText(/Anchor Text/i);
+    expect(anchorElement).toBeInTheDocument();
+    expect(anchorElement).toHaveAttribute('href', 'https://example.com');
+    expect(anchorElement).toHaveAttribute('title', 'Example Website');
+    expect(anchorElement).toHaveAttribute('aria-label', 'Learn more about Example Website');
+  });
+
+  it('should not render the anchor link if href is not provided', () => {
+    render(<Button>Click on me</Button>);
+    const anchorElement = screen.queryByTestId("anchor");
+    expect(anchorElement).not.toBeInTheDocument();
+  });
+
+  it('should not render the anchor link if children is not provided', () => {
+    render(<Button href="#" />);
+    const anchorElement = screen.queryByTestId("anchor");
+    expect(anchorElement).not.toBeInTheDocument();
+  });
+
+  it('should not behave like a button when needed', () => {
+    render(<Button href="https://example.com" isLink={true}>Download Now</Button>);
+    const anchorElement = screen.getByRole("link");
+    expect(anchorElement).toBeInTheDocument();
+    expect(anchorElement).toHaveAttribute('role', 'link');
+  });
+
+});
--- react-app/src/components/Button.tsx
+++ react-app/src/components/Button.tsx
@@ -1,8 +1,28 @@
-import type { FC } from 'react';
+import styled from 'styled-components';
+import type { FC, PropsWithChildren } from 'react';
 
-interface ButtonProps { }
-
-const Button: FC<ButtonProps> = ({ }) => {
-  return <button></button>
+interface AnchorProps {
+  title?: string;
+  ariaLabel?: string;
+  href?: string;
+  isLink?: boolean;
+  target?: string;
 }
-export default Button;
+
+const StyledAnchor = styled.a<AnchorProps>``;
+
+const AnchorLink: FC<PropsWithChildren & AnchorProps> = (props) => {
+  const { title, ariaLabel, target, href, children, isLink } = props;
+  const ariaRole = isLink ? "link" : "button";
+
+  if (!children || !href) return <></>;
+  return <StyledAnchor
+    data-testid="anchor"
+    target={target}
+    href={href}
+    aria-label={ariaLabel}
+    role={ariaRole}
+    title={title}>{children}</StyledAnchor>
+}
+
+export default AnchorLink;
