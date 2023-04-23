bfff73fd1deea26c8b80545450fd269c5b39f3d7
Junio Vitorino <jgvitorino@gmail.com>
2023-04-23 09:37:06 -0300

    feat: Add 10up Logo component

---
+++ react-app/src/components/TenUpLogo.spec.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from '@testing-library/react';
+import TenUpLogo from './TenUpLogo'
+
+describe('10up Logo', () => {
+  beforeEach(() => render(<TenUpLogo />));
+  it('should render the 10up logo', () => {
+    expect(screen.queryByTestId('tenUpLogo')).toBeInTheDocument();
+  });
+});
