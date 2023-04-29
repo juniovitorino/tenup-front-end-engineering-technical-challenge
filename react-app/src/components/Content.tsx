1961347aaf27646390322a9612584f532abcdfc2
Junio Vitorino <jgvitorino@gmail.com>
2023-04-29 11:45:42 -0300

    fix: Fix content on mobile

--- react-app/src/components/Content.tsx
+++ react-app/src/components/Content.tsx
@@ -9,28 +9,39 @@ interface ContentSectionProps {
 }
 
 const StyledContent = styled.section<ContentSectionProps>`
-  max-width: 1440px;
-  margin: 0 auto;
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  flex-direction: column;
   color: var(--${(p: ContentSectionProps) => p.section}-text);
-  padding: 10rem 9.6rem;
+  padding: 4rem 5rem;
+
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+    max-width: 1440px;
+    margin: 0 auto;
+    align-items: center;
+    padding: 10rem 3rem;
+    justify-content: space-around;
+  }
 `
-interface ColumnProps {
-  "max-width"?: string
-}
-const StyledColumn = styled.div<ColumnProps>`
-  max-width: ${(p: ColumnProps) => p["max-width"]};
+const StyledImage = styled.img`
+  width: 100%;
 `
+
+const StyledColumn = styled.div`
+
+  @media only screen and (min-width: 768px) {
+    max-width: 41%;
+  }
+
+  `
 
 const Content: FC<ContentSectionProps> = (props) => {
   return (
     <StyledContent data-testid="content-section">
       <StyledColumn>
-        <img src="/public/images/device-mobile.png" />
+        <StyledImage src="/public/images/device-mobile.png" alt="Mobile Phone with lines of code" />
       </StyledColumn>
-      <StyledColumn max-width="41%">
+      <StyledColumn>
         <SectionSubtitle {...props}>Eyebrow</SectionSubtitle>
         <SectionTitle {...props}>Generating relevant & engaging content</SectionTitle>
         <Paragraph {...props}>Non diam phasellus vestibulum lorem sed. Massa enim nec dui nunc mattis enim. Quisque egestas diam in arcu cursus euismod quis viverra. Eget arcu dictum varius duis at consecteur lorem donec massa.</Paragraph>
